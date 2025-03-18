e6c5601dc1e2662d08d7fcb8621064764dbbe19f
Jasmin Angcon <j.angcon.535761@umindanao.edu.ph>
2025-03-18 17:10:17 +0800

    Completed Lab 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,69 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowBookInfo = new Button();
+            btnShowList = new Button();
+            listBoxBooks = new ListBox();
+            SuspendLayout();
+            // 
+            // btnShowBookInfo
+            // 
+            btnShowBookInfo.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnShowBookInfo.Location = new Point(37, 92);
+            btnShowBookInfo.Name = "btnShowBookInfo";
+            btnShowBookInfo.Size = new Size(135, 29);
+            btnShowBookInfo.TabIndex = 0;
+            btnShowBookInfo.Text = "Show Book Info";
+            btnShowBookInfo.UseVisualStyleBackColor = true;
+            btnShowBookInfo.Click += btnShowBookInfo_Click;
+            // 
+            // btnShowList
+            // 
+            btnShowList.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnShowList.Location = new Point(37, 140);
+            btnShowList.Name = "btnShowList";
+            btnShowList.Size = new Size(135, 29);
+            btnShowList.TabIndex = 1;
+            btnShowList.Text = "Show List";
+            btnShowList.UseVisualStyleBackColor = true;
+            btnShowList.Click += btnShowList_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.BackColor = SystemColors.Info;
+            listBoxBooks.ForeColor = SystemColors.WindowText;
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(210, 88);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(605, 284);
+            listBoxBooks.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(888, 450);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnShowList);
+            Controls.Add(btnShowBookInfo);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowBookInfo;
+        private Button btnShowList;
+        private ListBox listBoxBooks;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,86 @@
+using System.Windows.Forms;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string? Title { get; set; }
+            public string? Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} | Issue: {IssueNumber}";
+            }
+        }
+
+        public class Ebook : Book
+        {
+            public string? Platform { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} | Platform: {Platform}";
+            }
+        }
+
+        public class TextBook : Book
+        {
+            public string? Subject { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} | Subject: {Subject}";
+            }
+        }
+
+        public class Audiobook : Book
+        {
+            public string? Narrator { get; set; }
+            public int Duration { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} | Narrated by: {Narrator} | Duration: {Duration} minutes";
+            }
+        }
+
+        private void btnShowBookInfo_Click(object sender, EventArgs e)
+        {
+            Book myBook = new Magazine()
+            {
+                Title = "Tech Weekly",
+                Author = "John Doe",
+                IssueNumber = 45
+            };
+            MessageBox.Show(myBook.GetInfo());
+        }
+
+        private void btnShowList_Click(object sender, EventArgs e)
+        {
+            Book[] books = {
+                new Magazine() { Title = "Sports Illustrated", Author = "Editorial Board", IssueNumber = 98 },
+                new Ebook() { Title = "Digital Marketing 101", Author = "Lisa Green", Platform = "Google Books" },
+                new TextBook() { Title = "Software Design", Author = "Andrew Bell", Subject = "Computer Science" },
+                new Audiobook() { Title = "The Art of War", Author = "Sun Tzu", Narrator = "Morgan Freeman", Duration = 200 }
+            };
+
+            listBoxBooks.Items.Clear();
+            foreach (var book in books)
+            {
+                listBoxBooks.Items.Add(book.GetInfo());
+            }
+        }
+    }
 }
