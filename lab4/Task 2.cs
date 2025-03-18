c22055bdd056712cfa2b1e3d7be4724a527c9713
Jasmin Angcon <j.angcon.535761@umindanao.edu.ph>
2025-03-18 17:31:15 +0800

    Completed Lab 4

---
+++ lab4/Task 2.cs
@@ -0,0 +1,27 @@
+﻿using lab4.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab4
+{
+    internal class Task_2
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidMagazineCreation()
+        {
+            var magazine = new Magazine()
+            {
+                Title = "Invalid",
+                Author = "Jane Doe",
+                IssueNumber = -1
+            };
+            magazine.CheckValidity();
+        }
+    }
+}
