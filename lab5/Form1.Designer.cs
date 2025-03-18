977929eda9a72fdcfc97fa82ccbd0060b2e5f442
Jasmin Angcon <j.angcon.535761@umindanao.edu.ph>
2025-03-18 18:35:05 +0800

    Completed Lab 5

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -1,39 +1,217 @@
 ﻿namespace lab5
 {
-	partial class Form1
-	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            btnSearchBook = new Button();
+            btnDeleteBook = new Button();
+            txtBookID = new TextBox();
+            btnUpdateBook = new Button();
+            btnShowBooks = new Button();
+            btnAddBook = new Button();
+            listBoxBooks = new ListBox();
+            txtBookTitle = new TextBox();
+            txtAuthorName = new TextBox();
+            txtSearch = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(23, 185);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 20);
+            label4.TabIndex = 25;
+            label4.Text = "Book ID:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 20);
+            label3.TabIndex = 24;
+            label3.Text = "Book Title:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 20);
+            label2.TabIndex = 23;
+            label2.Text = "Book Author:";
+            // 
+            // btnSearchBook
+            // 
+            btnSearchBook.Location = new Point(688, 35);
+            btnSearchBook.Name = "btnSearchBook";
+            btnSearchBook.Size = new Size(80, 29);
+            btnSearchBook.TabIndex = 22;
+            btnSearchBook.Text = "Search";
+            btnSearchBook.UseVisualStyleBackColor = true;
+            btnSearchBook.Click += btnSearchBook_Click;
+            // 
+            // btnDeleteBook
+            // 
+            btnDeleteBook.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnDeleteBook.Location = new Point(127, 338);
+            btnDeleteBook.Name = "btnDeleteBook";
+            btnDeleteBook.Size = new Size(125, 29);
+            btnDeleteBook.TabIndex = 21;
+            btnDeleteBook.Text = "Delete Book";
+            btnDeleteBook.UseVisualStyleBackColor = true;
+            btnDeleteBook.Click += btnDeleteBook_Click;
+            // 
+            // txtBookID
+            // 
+            txtBookID.Location = new Point(127, 182);
+            txtBookID.Name = "txtBookID";
+            txtBookID.Size = new Size(125, 27);
+            txtBookID.TabIndex = 20;
+            // 
+            // btnUpdateBook
+            // 
+            btnUpdateBook.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnUpdateBook.Location = new Point(127, 303);
+            btnUpdateBook.Name = "btnUpdateBook";
+            btnUpdateBook.Size = new Size(125, 29);
+            btnUpdateBook.TabIndex = 19;
+            btnUpdateBook.Text = "Update Book";
+            btnUpdateBook.UseVisualStyleBackColor = true;
+            btnUpdateBook.Click += btnUpdateBook_Click;
+            // 
+            // btnShowBooks
+            // 
+            btnShowBooks.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnShowBooks.Location = new Point(127, 268);
+            btnShowBooks.Name = "btnShowBooks";
+            btnShowBooks.Size = new Size(125, 29);
+            btnShowBooks.TabIndex = 18;
+            btnShowBooks.Text = "Show Book";
+            btnShowBooks.UseVisualStyleBackColor = true;
+            btnShowBooks.Click += btnShowBooks_Click;
+            // 
+            // btnAddBook
+            // 
+            btnAddBook.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnAddBook.Location = new Point(127, 233);
+            btnAddBook.Name = "btnAddBook";
+            btnAddBook.Size = new Size(125, 29);
+            btnAddBook.TabIndex = 17;
+            btnAddBook.Text = "Add Book";
+            btnAddBook.UseVisualStyleBackColor = true;
+            btnAddBook.Click += Add_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.BackColor = SystemColors.Info;
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(269, 105);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(499, 264);
+            listBoxBooks.TabIndex = 16;
+            // 
+            // txtBookTitle
+            // 
+            txtBookTitle.Location = new Point(127, 145);
+            txtBookTitle.Name = "txtBookTitle";
+            txtBookTitle.Size = new Size(125, 27);
+            txtBookTitle.TabIndex = 15;
+            // 
+            // txtAuthorName
+            // 
+            txtAuthorName.Location = new Point(127, 105);
+            txtAuthorName.Name = "txtAuthorName";
+            txtAuthorName.Size = new Size(125, 27);
+            txtAuthorName.TabIndex = 14;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(269, 35);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(413, 27);
+            txtSearch.TabIndex = 26;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(158, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 20);
+            label1.TabIndex = 27;
+            label1.Text = "Search Book:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(824, 434);
+            Controls.Add(label1);
+            Controls.Add(txtSearch);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btnSearchBook);
+            Controls.Add(btnDeleteBook);
+            Controls.Add(txtBookID);
+            Controls.Add(btnUpdateBook);
+            Controls.Add(btnShowBooks);
+            Controls.Add(btnAddBook);
+            Controls.Add(listBoxBooks);
+            Controls.Add(txtBookTitle);
+            Controls.Add(txtAuthorName);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Lab 5 - Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button btnSearchBook;
+        private Button btnDeleteBook;
+        private TextBox txtBookID;
+        private Button btnUpdateBook;
+        private Button btnShowBooks;
+        private Button btnAddBook;
+        private ListBox listBoxBooks;
+        private TextBox txtBookTitle;
+        private TextBox txtAuthorName;
+        private TextBox txtSearch;
+        private Label label1;
+    }
 }
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -1,10 +1,198 @@
+using Microsoft.EntityFrameworkCore;
 namespace lab5
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Author
+        {
+            public int AuthorID { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public int AuthorID { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+        public void AddAuthorWithBook(string authorName, string bookTitle)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var author = new Author { Name = authorName };
+                var book = new Book { Title = bookTitle, Author = author };
+
+                context.Authors.Add(author);
+                context.Books.Add(book);
+                context.SaveChanges();
+            }
+        }
+
+        public List<string> GetBooksWithAuthors()
+        {
+            using (var context = new BookstoreContext())
+            {
+                return context.Books
+                    .Include(book => book.Author)
+                    .Select(book => $"[{book.BookID}] {book.Title} by {book.Author.Name}")
+                    .ToList();
+            }
+        }
+
+        public List<string> SearchBooks(string searchText)
+        {
+            using (var context = new BookstoreContext())
+            {
+                return context.Books
+                    .Include(b => b.Author)
+                    .Where(b => b.Title.Contains(searchText) || b.Author.Name.Contains(searchText))
+                    .Select(b => $"[{b.BookID}] {b.Title} by {b.Author.Name}")
+                    .ToList();
+            }
+        }
+
+        public bool UpdateBookAndAuthor(int bookId, string newTitle, string newAuthorName)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = context.Books
+                    .Include(b => b.Author)
+                    .FirstOrDefault(b => b.BookID == bookId);
+
+                if (book == null) return false;
+
+                book.Title = newTitle;
+                book.Author.Name = newAuthorName;
+                return context.SaveChanges() > 0;
+            }
+        }
+
+        public bool DeleteBook(int bookId)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = context.Books.Find(bookId);
+                if (book == null) return false;
+
+                context.Books.Remove(book);
+                return context.SaveChanges() > 0;
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+            txtAuthorName.Clear();
+            txtBookTitle.Clear();
+            txtBookID.Clear();
+            txtSearch.Clear();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            string authorName = txtAuthorName.Text;
+            string bookTitle = txtBookTitle.Text;
+
+            if (string.IsNullOrWhiteSpace(authorName) || string.IsNullOrWhiteSpace(bookTitle))
+            {
+                MessageBox.Show("Please enter both the author's name and the book title.");
+                return;
+            }
+
+            try
+            {
+                AddAuthorWithBook(authorName, bookTitle);
+                MessageBox.Show("Book added successfully!");
+                ClearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = $"Error: {ex.Message}";
+                if (ex.InnerException != null)
+                {
+                    errorMessage += $"\nInner Error: {ex.InnerException.Message}";
+                }
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        private void btnShowBooks_Click(object sender, EventArgs e)
+        {
+            var books_authors = GetBooksWithAuthors();
+            listBoxBooks.DataSource = books_authors;
+        }
+
+        private void btnUpdateBook_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtBookID.Text, out int bookId))
+            {
+                MessageBox.Show("Please enter a valid numeric Book ID");
+                return;
+            }
+
+            try
+            {
+                bool updated = UpdateBookAndAuthor(bookId, txtBookTitle.Text, txtAuthorName.Text);
+                if (updated)
+                {
+                    MessageBox.Show("Book updated successfully!");
+                    ClearTextBoxes();
+                    var updatedBooks = GetBooksWithAuthors();
+                    listBoxBooks.DataSource = null;
+                    listBoxBooks.DataSource = updatedBooks;
+                }
+                else
+                {
+                    MessageBox.Show("No book found with ID: " + bookId);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating book: {ex.Message}\n{ex.InnerException?.Message}");
+            }
+        }
+
+        private void btnDeleteBook_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtBookID.Text, out int bookId))
+            {
+                MessageBox.Show("Please enter a valid numeric Book ID");
+                return;
+            }
+
+            try
+            {
+                bool deleted = DeleteBook(bookId);
+                if (deleted)
+                {
+                    MessageBox.Show("Book deleted successfully!");
+                    ClearTextBoxes();
+                    var updatedBooks = GetBooksWithAuthors();
+                    listBoxBooks.DataSource = null;
+                    listBoxBooks.DataSource = updatedBooks;
+                }
+                else
+                {
+                    MessageBox.Show("No book found with ID: " + bookId);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting book: {ex.Message}\n{ex.InnerException?.Message}");
+            }
+        }
+
+        private void btnSearchBook_Click(object sender, EventArgs e)
+        {
+            string searchText = txtSearch.Text;
+            var searchResults = SearchBooks(searchText);
+            listBoxBooks.DataSource = searchResults;
+        }
+    }
 }
